b324ccc8145893c07c5f5f6a32f05df4e770872d
Gunawan Puspo Prajoko <gupuspo@gmail.com>
2026-08-22 19:53:34 +0700

    test: add health check test

---
+++ tests/health.test.js
@@ -0,0 +1,16 @@
+const request = require("supertest")
+const app = require("../src/app")
+
+describe("GET /health", () => {
+  it("should return application health status", async () => {
+    const response = await request(app).get("/health")
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({
+      success: true,
+      data: {
+        status: "ok"
+      }
+    })
+  })
+})

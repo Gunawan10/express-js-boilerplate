f79d7af6c0cdd8e6205acb442f8c15ed87283791
Gunawan Puspo Prajoko <gupuspo@gmail.com>
2026-08-22 19:49:03 +0700

    feat: add security middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+const compression = require("compression")
+const rateLimit = require("express-rate-limit")
 
 const app = express()
 
+app.disable("x-powered-by")
+
+app.use(helmet())
+app.use(cors())
+app.use(compression())
 app.use(express.json())
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    standardHeaders: "draft-8",
+    legacyHeaders: false
+  })
+)
 
 app.use("/users", require("./resources/users/routes"))
 app.use("/ai", require("./resources/ai/routes"))
